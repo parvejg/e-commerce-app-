899f949cd046e66c23fbe3aa5c375503e1b3d47f
parvejg <parvezpg321@gmail.com>
2023-07-05 20:57:54 +0530

    navbar complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+<Navbar/>       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import "./Navbar.css";
+import { BsCart } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+export const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <h3 className="flipcart-logo">Flipcart</h3>
+      <nav className="navbar">
+        <input className="navbar-input" type="text" />
+        <ul className="unorderedList-wrapper">
+          <li>Login</li>
+          <li>Sin Up</li>
+        </ul>
+        <ul className="navbar-icons-wrapper">
+          <li>Cart</li>
+          <BsCart className="cart-icon" />
+          <li>Wishlist</li>
+          <AiOutlineHeart className="wishlist-icon" />
+        </ul>
+      </nav>
+    </div>
+  );
+};
