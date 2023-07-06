48614f7bed6cdbb47a9acd6cb13187b9fbb06f0f
parvejg <parvezpg321@gmail.com>
2023-07-06 22:54:14 +0530

    add cart and wishlist

---
+++ src/Components/Badge.js
@@ -0,0 +1,24 @@
+import { AiOutlineHeart } from "react-icons/ai";
+import {BsCart} from "react-icons/bs";
+export const Cart = () => {
+  return (
+    <span>
+      <a className="navbar-links" href="/">
+        <AiOutlineHeart className="wishlist-icon" />
+        <small className="wishlist-icon-wrapper">
+          <span className="unitOfWishlist">13</span>
+        </small>
+      </a>
+    </span>
+  );
+};
+export const wishlist = () => {
+    return (
+      <span>
+       <a className="navbar-links" href="/">
+              <BsCart className="cart-icon" />
+             <small className="cart-icon-wrapper"><span className="unitOfCart">15</span></small> 
+            </a>{" "}
+      </span>
+    );
+  };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,38 @@
 import "./Navbar.css";
+import { ImSearch } from "react-icons/im";
 import { BsCart } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 export const Navbar = () => {
   return (
-    <div className="navbar-wrapper">
-      <h3 className="flipcart-logo">Flipcart</h3>
-      <nav className="navbar">
+    <nav className="navbar-wrapper">
+      <h3 className="flipcart-text"> Flipcart </h3>{" "}
+      <div className="navbar-input-wrapper">
         <input className="navbar-input" type="text" />
-        <ul className="unorderedList-wrapper">
-          <li>Login</li>
-          <li>Sin Up</li>
-          <li>Cart</li>
-          <BsCart className="cart-icon" />
-          <li>Wishlist</li>
-          <AiOutlineHeart className="wishlist-icon" />
-        </ul>
-
-      </nav>
-    </div>
+       <span className="search-icon-wrapper"><a className="search-icon" href="/">
+          <ImSearch />
+        </a></span> 
+      </div>{" "}
+      <div className="navbar-links-wrapper">
+        <li>
+          <a className="navbar-links sign-up" href="/">
+            Sign In{" "}
+          </a>{" "}
+        </li>{" "}
+        <div className="navbar-icon-links-wrapper">
+          <li>
+            <a className="navbar-links" href="/">
+              <BsCart className="cart-icon" />
+             <small className="cart-icon-wrapper"><span className="unitOfCart">15</span></small> 
+            </a>{" "}
+          </li>{" "}
+          <li>
+            <a className="navbar-links" href="/">
+              <AiOutlineHeart className="wishlist-icon" />
+              <small className="wishlist-icon-wrapper"><span className="unitOfWishlist">13</span></small> 
+            </a>{" "}
+          </li>{" "}
+        </div>{" "}
+      </div>{" "}
+    </nav>
   );
 };
