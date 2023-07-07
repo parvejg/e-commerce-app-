f0b92b3040721e38ea70cf03fa3627e1f2a536d9
parvejg <parvezpg321@gmail.com>
2023-07-07 20:25:54 +0530

    complete icon footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Footer } from "./Components/Footer";
+import { Footer, WebFooter } from "./Components/Footer";
 import { Navbar } from "./Components/Navbar";
 import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-{/* <Navbar/>     */}
-<Footer/>   
+<Navbar/>    
+<WebFooter/>   
     </div>
   );
 }
 
 export default App;
+  
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,8 @@
 import "./Footer.css";
+import {FaFacebook , FaGithub} from "react-icons/fa";
+import {RiInstagramFill} from "react-icons/ri";
+import { AiFillLinkedin } from "react-icons/ai";
+
 export const  Footer =()=>{
     return <div className="footer-wrapper">
         <div className="footer-links-wrapper">
@@ -6,6 +10,17 @@ export const  Footer =()=>{
             <a className="footer-links" href="/">Instagram</a>
             <a className="footer-links" href="/">Github</a>
             <a className="footer-links" href="/">LinkedIn</a>
+        </div>
+    </div>
+}
+
+export const  WebFooter =()=>{
+    return <div className="webfooter-wrapper">
+        <div className="webfooter-links-wrapper">
+            <a className="webfooter-links" href="/"><FaFacebook/></a>
+            <a className="webfooter-links" href="/"><RiInstagramFill/></a>
+            <a className="webfooter-links" href="/"><FaGithub/></a>
+            <a className="webfooter-links" href="/"><AiFillLinkedin/></a>
         </div>
     </div>
 }
