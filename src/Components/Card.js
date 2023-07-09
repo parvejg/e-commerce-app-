58d43cd34690e6e50a367613522613ac98dc223a
parvejg <parvezpg321@gmail.com>
2023-07-09 06:24:01 +0530

    card complete

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,8 @@
 import "./Card.css";
-export const ProductDemoCard = (props) => {
+export const ProductDemoCard1 = (props) => {
     const { src, alt } = props;
     return (
-      <div className="product-demo-container">
+      <selection className="product-demo-container">
         <div className="product-img-container">
           <img
             className="product-img"
@@ -24,6 +24,29 @@ export const ProductDemoCard = (props) => {
           <button className="buy-now-btn">Buy Now</button>
           <button className="cart-btn">Add To Cart</button>
         </div>
-      </div>
+      </selection>
     );
   };
+
+  export const ProductDemoCard2=()=>{
+    return <div className="card2-container">
+      <div className="productCard2-wrapper">
+        <div className="productCard2">
+          <img className="cycle-img2" src="https://media.istockphoto.com/id/968910650/photo/world-environment-day-and-save-environment-concept-volunteer-women-holding-plant-growing.jpg?s=612x612&w=0&k=20&c=J8MU9G0KdD7Qs8wshJstRw15tV1dd9zIFGlHetehRj0="/>
+          <a href="/" className='product-categories'>Annual plant</a>
+        </div>
+      </div>
+    </div>
+  }
+  export const ProductDemoCard3=()=>{
+    return <div className="card-container">
+      <div className="productCard-wrapper">
+        <div className="productCard">
+          <img className="cycle-img" src="https://images.unsplash.com/photo-1493723843671-1d655e66ac1c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGUlMjBjb21tZXJjZSUyMHByb2R1Y3R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"/>
+          <p>Product name and brand</p>
+          <h6 className="product-price">product price</h6>
+          <a  className="buyNow-btn"href="/">Buy Now</a>
+        </div>
+      </div>
+    </div>
+  }
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,14 +2,16 @@ import { WebFooter } from "./Footer";
 import { Navbar, NavbarLinks } from "./Navbar";
 import "./Layout.css";
 import { TopWebShoesImg } from "./Images";
-import { ProductDemoCard } from "./Card";
+import { ProductDemoCard1, ProductDemoCard2, ProductDemoCard3 } from "./Card";
 export const Layout = ({ children }) => {
   return (
     <div className="layout-wrapper">
       <Navbar />
       <NavbarLinks/>
       <TopWebShoesImg/>
-      <ProductDemoCard/>
+      <ProductDemoCard1/>
+      <ProductDemoCard2/>
+      <ProductDemoCard3/>
       <div className="layout-body">
         <div className="layout-content">{children}</div>
       </div>
