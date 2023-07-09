1851e5294e47b39e8d26b809a41495cce34ebd75
parvejg <parvezpg321@gmail.com>
2023-07-10 00:56:08 +0530

    slider image complete

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,6 +1,9 @@
 import "./Images.css"
 export const TopWebShoesImg=()=>{
     return (
-        <img className="shoes-image"  alt="shoes img"src="https://thumbs.dreamstime.com/b/blue-shoes-29507491.jpg"/>
+        <div className="slider-img-wrapper">
+ <div className="slider-img">
+        </div>
+        </div>
     )
 }
