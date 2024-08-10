6a50b710b079a1067c02b360da4ba13fe14570df
parvejg <parvezpg321@gmail.com>
2024-08-10 22:43:19 +0530

    create a personal information page for customer personal info

---
+++ src/Components/PersonalInformation.js
@@ -0,0 +1,10 @@
+ import "./PersonalInformation.css"
+ export const PersonalInfo = ()=>{
+    return <div>
+            <div className="personal-info">
+              <h2>personal information</h2>
+              <p className="name-txt"> Name: Parvez</p>
+              <p className="gmail-txt"> Gmail: xyz@gmail.com</p>
+            </div>
+    </div>
+}
